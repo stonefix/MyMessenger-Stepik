5b4b711edfaaec03d78f00a509f56ccbca17cc44
Yura <stonefiz@gmail.com>
2021-07-15 03:04:13 +0300

    start

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,7 +6,7 @@ namespace MyMessenger_Stepik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start!");
         }
     }
 }
